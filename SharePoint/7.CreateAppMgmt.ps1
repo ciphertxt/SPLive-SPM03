e6f087678eb5cb3d9bf8384ff51d29b7601fa79a
ciphertxt <scottmhoag@gmail.com>
2014-05-14 20:08:46 +1000

    Create 7.CreateAppMgmt.ps1

---
+++ SharePoint/7.CreateAppMgmt.ps1
@@ -0,0 +1,25 @@
+$subSettingsSvc = Get-SPServiceInstance | ? { $_.TypeName -eq "Microsoft SharePoint Foundation Subscription Settings Service" 
+$appMgmtSvc = Get-SPServiceInstance | ? { $_.TypeName -eq "App Management Service" } 
+
+Start-SPServiceInstance $subSettingsSvc
+Start-SPServiceInstance $appMgmtSvc
+
+# Creates the Subscription Settings service application, using the variable to associate it with the application pool that was created earlier.
+# Stores the new service application as a variable for later use.
+$svcAppPool = Get-SPServiceApplicationPool "SharePoint Web Services Default"
+$appSubSvc = New-SPSubscriptionSettingsServiceApplication -ApplicationPool $svcAppPool -Name "Subscription Settings Service Application" -DatabaseName "ESDEVWFE01_SA_SubscriptionSettings"
+
+# Creates a proxy for the Subscription Settings service application.
+$proxySubSvc = New-SPSubscriptionSettingsServiceApplicationProxy -ServiceApplication $appSubSvc
+
+# Creates the Application Management service application, using the variable to associate it with the application pool that was created earlier.
+# Stores the new service application as a variable for later use.
+$appAppSvc = New-SPAppManagementServiceApplication -ApplicationPool $svcAppPool -Name "App Management Service Application" -DatabaseName "ESDEVWFE01_SA_AppManagement"
+
+# Creates a proxy for the Application Management service application.
+$proxyAppSvc = New-SPAppManagementServiceApplicationProxy -ServiceApplication $appAppSvc
+
+# Set the base domain
+Set-SPAppDomain <appDomain>
+# Set the app prefix
+Set-SPAppSiteSubscriptionName -Name "app" -Confirm:$false
